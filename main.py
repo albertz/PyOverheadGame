aa728494741c5f99cbd64a0b25e5ba941eae6a78
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2017-07-08 15:05:12 +0200

    initial main

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+try:
+    import arcade
+    import better_exchook
+except ImportError:
+    print("See requirements.txt or README.md about what you need to install.")
+    print("Usually: pip3 --user -r requirements.txt")
+    print()
+    raise
+
+
+if __name__ == "__main__":
+    better_exchook.install()
+    
